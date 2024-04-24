0d6dcf552546e89c67b70441b8aab4302e0838a6
farjanaIslamNipa <nipa.farzana87@gmail.com>
2024-04-24 22:46:17 +0600

    working on single product page, added react image gallery npm package

--- src/app/(web)/products/[productId]/page.tsx
+++ src/app/(web)/products/[productId]/page.tsx
@@ -1,3 +1,23 @@
+'use client'
+import ImageGallery from "react-image-gallery";
+import 'react-image-gallery/styles/css/image-gallery.css'; 
+
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
+
+
 interface TProductId {
   params: {
     productId: string;
@@ -5,8 +25,21 @@ interface TProductId {
 }
 const SingleProduct = ({params} : TProductId) => {
   return (
-    <div className="custom-container">
+    <div className="custom-container pt-10 pb-10">
       single product {params.productId}
+      <div className="grid grid-cols-2">
+        <div className="">
+          <ImageGallery
+            showFullscreenButton={false}
+            showPlayButton={false}
+            showNav={false}
+            thumbnailPosition='left'
+            items={images} />
+        </div>
+        <div>
+          
+        </div>
+      </div>
     </div>
   );
 };
